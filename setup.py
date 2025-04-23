fc34ac04fdee969ae20b99a77409a041a5f0bf89
Aditya Chandramauli <adityachandramauli719@gmail.com>
2025-04-23 14:24:47 +0530

    notebook

--- setup.py
+++ setup.py
@@ -1,29 +1,28 @@
+
 from setuptools import find_packages,setup
 from typing import List
- 
-HYPNE_E_DOT="-e ."
 
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    this function will return the list of requirments
- 
+    this function will return the list of requirements
     '''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n"," ") for req in requirements]
-                
-        if HYPNE_E_DOT in requirements:
-           requirements.remove(HYPNE_E_DOT)
-                
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
 
 setup(
-    name="mlproject",
-    version="0.0.1",
-    author="Aditya",
-    author_email="adityachandamauli719@gmail.com",
-    packages=find_packages(),
-    install_requires=get_requirements("requirements.txt")
-    
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
 
 )
--- src/exceptions.py
+++ src/exceptions.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+import src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -8,7 +8,7 @@ def error_message_detail(error,error_detail:sys):
         file_name,exc_tb.tb_lineno,str(error))
 
 
-        return error_message
+    return error_message
     
 
 
@@ -21,13 +21,11 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message    
     
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Logging has started")  
-        raise CustomException(e,sys)    
-      
 
+
+    
+
+    
+    
 
 
